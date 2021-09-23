56823418afbcf54466f3be185324a23a17063b19
K4nishk <k4nishk@github.com>
2021-09-22 18:51:10 -0600

    tweaks and print statements removed

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver, os
+from urllib.parse import unquote
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,7 +29,6 @@ import socketserver, os
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    # /../ for web browser case???
 
     def buildResponse(self, protocol, status, filepath):
         """
@@ -44,9 +44,11 @@ class MyWebServer(socketserver.BaseRequestHandler):
         RETURNS:
             * response: A string which contains the HTTP Header data and the HTML document.
         """
+
         response = ""
         if status.split()[0] == "200":
             content, contentLength = self.getContent(filepath)
+
             # Resolved the case of connection staying intact after the code response was sent using Martin's explanation.
             # https://stackoverflow.com/questions/61880928/what-does-connection-0-to-host-example-com-left-intact-mean
             response = protocol + " " + status + self.getContentType(filepath) + contentLength + "Connection: closed\r\n\r\n" + content
@@ -54,11 +56,13 @@ class MyWebServer(socketserver.BaseRequestHandler):
             content, contentLength = self.getContent(filepath)
             response = protocol + " " + status + self.getContentType(filepath) + contentLength + "Connection: closed\r\n\r\n" + content
         else:
-            error, errorLength = self.buildErrorResponse(status)
+            error, errorLength = self.buildErrorPage(status)
             response = protocol + " " + status + errorLength + "Content-Type: text/html; charset=UTF-8\r\n" + "Connection: closed\r\n\r\n" + error
+
         return response
 
-    def buildErrorResponse(self, status):
+
+    def buildErrorPage(self, status):
         """
         FUNCTIONALITY:
             * This method is creates a separate HTML page for the error responses that need to be returned such as 404.
@@ -71,6 +75,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
             * errorInHtml: A string which contains the HTML document.
             * errorLength: A string converted int which contains the length of the data inside the HTML page, i.e. errorInHtml
         """
+
         htmlHeader = "<!DOCTYPE html>\n<head><meta charset='UTF-8'></head>\n<html>\n<body>\n"
         htmlFooter = "</body>\n</html>"
 
@@ -78,6 +83,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
         errorLength = "Content-Length: " + str(len(errorInHtml)) + "\r\n"
 
         return errorInHtml, str(errorLength)
+
 
     def getContentType(self, filepath):
         """
@@ -89,17 +95,22 @@ class MyWebServer(socketserver.BaseRequestHandler):
             * filepath: It is the path to the file requested from the root directory.
 
         RETURNS:
-            * contentType: A string that returns the file type along with the appended HTTP header info. 
+            * contentType: A string that returns the file type along with the appended HTTP header info and CLRF. 
         """
+
         contentType = "Content-type: "
         if "." not in filepath:
-            contentType = contentType + "text/plain; charset=UTF-8"
+            contentType = contentType + "application/octet-stream"
+            # contentType = contentType + "text/plain; charset=UTF-8"
+        
         else:
             if filepath[filepath.find(".") + 1 :] == "html":
                 contentType = contentType + "text/html; charset=UTF-8"
             elif filepath[filepath.find(".") + 1 :] == "css":
                 contentType = contentType + "text/css; charset=UTF-8"
+        
         return contentType + "\r\n"
+
 
     def getContent(self, filepath):
         """
@@ -111,9 +122,10 @@ class MyWebServer(socketserver.BaseRequestHandler):
             * filepath: It is the path to the file requested from the root directory.
 
         RETURNS:
-            * content: A string that contains the data from the read file.
+            * content: A string that contains the data from the read file with appended CRLF.
             * contentLength: A string converted int that contains the length of the data inside the requested file. 
         """
+
         content = ""
         contentLength = "Content-Length: "
         try:
@@ -126,8 +138,12 @@ class MyWebServer(socketserver.BaseRequestHandler):
             return -1, -1
 
         file.close()
-        contentLength = contentLength + str(len(content)) + "\r\n"
+
+        # minus 2 because I append \r\n to the content string.
+        contentLength = contentLength + str(len(content) - 2) + "\r\n"
+        
         return content, contentLength
+
 
     def handle(self):
         """
@@ -141,13 +157,17 @@ class MyWebServer(socketserver.BaseRequestHandler):
         RETURNS:
             * None
         """
-        # print("=====")
-        self.data = self.request.recv(1024).strip()
 
+        self.data = self.request.recv(1024).strip()
+        print ("Got a request of: %s\n" % self.data)
+
+        print("=====")
         # Extract the GET request
-        request = self.data.decode("utf-8").split("\r\n")[0]
-        # Extract the info from the GET request
-        method, filepath, protocol = request.split(" ")
+        request = unquote(self.data.decode()).split("\r\n")[0]
+
+        method = request[:request.find(" /")]       # HTTP method
+        protocol = request[request.find("HTTP"):]   # the protocol
+        filepath = request.replace(method, "").replace(protocol,"").strip(" ")  # remove extras to get the requested path
         
         # If there is /../ in the GET request, we don't allow it.
         cFlag = True
@@ -159,68 +179,60 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
         status = ""
         if method != "GET":
-            print("Method not allowed. Return 405 status code")
+            # The request method is not GET so, return 405. 
             status = "405 Method Not Allowed\r\n"
         else:
-            print("GET request received")
-
             # os.path methods referenced from:
             # https://www.guru99.com/python-check-if-file-exists.html
             if cFlag:
-                print("Trying to access other directories. Return 404 status code.")
+                # Trying to access other directories. Return 404 status code.
                 status = "404 Not Found\r\n"
             else:
-                print("Allowed.")
                 if os.path.isdir(filepath):
-                    print("It is a directory")
+                    # It is a directory.
 
                     if filepath[-1] == "/":
-                        # If the last character of the filepath is "/", then we simply display the indxe.html in the directory
+                        # If the last character of the filepath is "/", then we simply display the index.html in the directory
                         filepath = filepath + "index.html"
                         if not (os.path.exists(filepath)):
-                            # If even after appending index.html the address requested is not found return 404
-                            print("Filepath does not exist. Return 404 status code")
+                            # If the directory is empty and thereby no index.html file is present. Return 404.
                             status = "404 Not Found\r\n"
                         else:
-                            # else the file is found and we send the data
-                            print("Sending data....")
+                            # Else the file is found and we send the data.
                             status = "200 OK\r\n"
                     else:
                         # Otherwise, we have to correct the path by appending a "/" and return a 301 code and do the same jazz as before.
-                        print("Redirection to the correct path. Return 301 status code")
                         filepath = filepath + "/index.html"
-                        status = "301 Moved Permanently\r\n"
+                        if not (os.path.exists(filepath)):
+                            # If the directory is empty and thereby no index.html file is present. Return 404.
+                            status = "404 Not Found\r\n"
+                        else:
+                            # Else the file is found and we send the data with 301 code.
+                            status = "301 Moved Permanently\r\n"
 
-                        # Send the response right now and make status an empty string so that the response isn't sent again
-                        response = self.buildResponse(protocol, status, filepath)
-                        self.request.sendall(bytearray(response, "utf-8"))
-                        print("Redirection successful.")
-                        status = ""                     
+                            # Send the response right now and make status an empty string so that the response isn't sent again
+                            response = self.buildResponse(protocol, status, filepath)
+                            self.request.sendall(bytearray(response, "utf-8"))
+                            status = ""                     
 
                 else:
+                    # It is a file
                     if not (os.path.exists(filepath)):
                         # If the file requested doesn't exist in the root directory.
-                        print("File requested can't be found")
                         status = "404 Not Found\r\n"
                     else:
-                        print("Filepath for the file is valid, proceed")
                         if os.path.isfile(filepath):
                             # Confirmt that the file indeed is a file
-                            print("Return file content")
                             status = "200 OK\r\n"
                         else:
                             # at this point there should be no cases left to handle but for safety reasons, return 404
-                            print("Unknown Error???")
                             status = "404 Not Found\r\n"
 
         if status != "":
             response = self.buildResponse(protocol, status, filepath) 
-            print("Send back requested data")
             self.request.sendall(bytearray(response, "utf-8"))
-            # print("-----")
+            print("-----")
 
-        print ("Got a request of: %s\n" % self.data)
-        # self.request.sendall(bytearray("OK",'utf-8'))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
